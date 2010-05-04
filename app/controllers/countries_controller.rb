9c52c801653b8e13a57df31eca97ba5ec5f07121
Andy Sturges <arsturges@lbl.gov>
2010-05-03 18:05:28 -0700

    added and removed autocomplete. total failure.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -7,6 +7,7 @@ class CountriesController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @countries }
+      format.json { render :json => @countries }
     end
   end
 
