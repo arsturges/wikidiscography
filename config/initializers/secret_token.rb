56cad41b42d9357c669427166adb7627290598d0
Andy Sturges <arsturges@lbl.gov>
2011-06-05 15:05:16 -0700

    initial commit of 3.1 (rc1)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikidiscography::Application.config.secret_token = 'd6e60774adcf1d9222b8f683a811d5140e1c8250d1a3dbba29d89a974eda97f40391193f66d0f689314158462c50524f3292ea0f04f18ec5377bcab283cf9c8e'
